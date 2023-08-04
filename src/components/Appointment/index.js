c1f98fed69c68216483475a80b6b4eeebafd3e8a
Shawn He <shawnhe1991@gmail.com>
2023-08-04 14:36:12 -0400

    add appointmnet module

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../Appointment/Header"
+import Empty from "./Empty";
+import Show from "./Show";
+
+export default function Appointment() {
+  return(
+    <article className="appointment">
+      </article>
+  )
+}
