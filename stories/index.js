642f270021ed6896eae6c2b5267cea46c12234c3
Shawn He <shawnhe1991@gmail.com>
2023-08-06 09:25:33 -0400

    complete storybook

--- stories/index.js
+++ stories/index.js
@@ -19,6 +19,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -196,5 +197,11 @@ storiesOf("Button", module)
         .add("Deleting", () => (
           <Status
           message="Deleting"
+          />
+        ))
+        .add("Error", () => (
+          <Error
+          message="Could not delete appointment."
+          onClose={action("onClose")}
           />
         ))
