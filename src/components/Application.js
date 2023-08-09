ac3f20236cfb25f7c502141d921404abbc905f6d
Shawn He <shawnhe1991@gmail.com>
2023-08-09 10:56:39 -0400

    add Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Button from "./Button";
 import DayList from "./DayList";
 import DayListItem from "./DayListItem";
-
+import Appointment from "./Appointment";
 
 import "components/Application.scss";
 
@@ -24,8 +24,47 @@ const days = [
   },
 ];
 
+const appointments = {
+  1: {
+    id: 1,
+    time: "12pm",
+  },
+  2: {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  3: {
+    id: 3,
+    time: "2pm",
+  },
+  4: {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  5: {
+    id: 5,
+    time: "4pm",
+  },
+};
+
 export default function Application(props) {
-  const [day, setDay] = useState("Monday")
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
@@ -37,11 +76,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList
-          days={days}
-          day = {day}
-          onChange={setDay}
-        />
+          <DayList days={days} day={day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -50,9 +85,11 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {Object.values(appointments).map((appointment) => (
+          <Appointment key={appointment.id} {...appointment} />
+        ))}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
 }
-
