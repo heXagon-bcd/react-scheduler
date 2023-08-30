68530ed2231899d981384fbfa61fa98d0c6d6afa
Shawn He <shawnhe1991@gmail.com>
2023-08-30 12:00:19 -0400

    add getInterviewForDay Func

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -27,3 +27,23 @@ return {
     interviewer: state.interviewers[interview.interviewer]
   }
 }
+
+
+export function getInterviewersForDay(state, day) {
+  console.log("state-Interviewers",state  )
+  console.log("state.interviewers", state.interviewers)
+  // Find the day in the state.days array
+  const selectDay = state.days.find(d => d.name === day);
+  console.log("selectDay",selectDay)
+  
+  // If the day doesn't exist, return an empty array
+  if (!selectDay) {
+    return [];
+  }
+  
+  // Map the day's appointments IDs to their corresponding appointment details in state.appointments
+  const InterviewersForDay = selectDay.interviewers.map(interviewerId => state.interviewers[interviewerId]);
+  console.log("InterviewersForDay",InterviewersForDay)
+  
+  return InterviewersForDay;
+  }
