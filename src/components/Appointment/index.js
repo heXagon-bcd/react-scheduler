6756356aabcc45da5b5eedeb0310ebbb53efb6f3
Shawn He <shawnhe1991@gmail.com>
2023-09-14 22:38:10 -0400

    complete integration scripts

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -67,7 +67,7 @@ export default function Appointment(props) {
   }
 
   return (
-    <article className="appointment">
+    <article className="appointment" data-testid="appointment">
       {mode === EMPTY && <Empty onAdd={() => {
         console.log("Clicked onAdd") 
       transition(CREATE)} } />
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -20,6 +20,7 @@ export default function useApplicationData(intial) {
       axios.get("/api/interviewers"),
     ]).then((all) =>{
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      console.log("State", state)
     })
   },[])
 
